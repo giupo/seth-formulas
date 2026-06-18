9eb8a253601c3a3b3709567cbb7fe56118906c3c
MasterOfPuppets <giuseppe.acito@bancaditalia.it>
2026-06-18 18:01:21 +0200

    adds meson

---
+++ meson.py
@@ -0,0 +1,29 @@
+from seth.formula import Formula
+from pathlib import Path
+
+import stat
+
+class MesonFormula(Formula):
+    name = "meson"
+    latest = "1.11.1"
+    # dependencies = []
+    # build_dependencies = []
+    build_system = "custom"
+    
+    versions = {
+        "1.11.1": {
+            "url": "https://github.com/mesonbuild/meson/releases/download/1.11.1/meson.pyz",
+            "sha256": "",
+        },
+    }
+        
+    def build(self, source_dir: Path):
+        # copy file and rename to meson
+        Path(self.keg / "bin").mkdir(parents=True, exist_ok=True)        
+        import shutil
+        src = source_dir / "meson.pyz"
+        dst = self.keg / "bin" / "meson"
+        shutil.copy2(src, dst)
+        dst.chmod(
+            dst.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+        )
