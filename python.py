a9ee6c363a7293fd6f79e8ca5dce01dc6867e7af
MasterOfPuppets <giuseppe.acito@gmail.com>
2026-06-19 12:50:48 +0200

    adds texinfo

--- python.py
+++ python.py
@@ -3,12 +3,12 @@ from seth.formula import Formula
 
 class PythonFormula(Formula):
     name = "python"
-    latest = "3.14.0"
+    latest = "3.14.6"
  
-    dependencies = ["bzip2", "xz", "libztsd", "libffi"]
+    dependencies = ["xz", "libztsd", "libffi"]
     
     versions = {
-        "3.14.0": {
+        "3.14.6": {
             "url": "https://www.python.org/ftp/python/3.14.6/Python-3.14.6.tar.xz",
             "sha256": "143b1dddefaec3bd2e21e3b839b34a2b7fb9842272883c576420d605e9f30c63",
         },
@@ -17,11 +17,11 @@ class PythonFormula(Formula):
     def configure_args(self):
         return [
             f"--prefix={self.keg}",
-            "--enable-optimizations",
+            # "--enable-optimizations",
             "--with-lto",
         ]
 
-    def make_args(self):
-        return [
-            "PROFILE_TASK='-m test --pgo --timeout=$(TESTTIMEOUT) -x test_json'",
-        ]
+    # def make_args(self):
+    #     return [
+    #         "PROFILE_TASK='-m test --pgo --timeout=$(TESTTIMEOUT) -x test_json'",
+    #     ]
