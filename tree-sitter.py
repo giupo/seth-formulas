063e65731ddc29e684244b410a2b1b9a56ce99f8
MasterOfPuppets <giuseppe.acito@bancaditalia.it>
2026-06-19 16:06:50 +0200

    adds tree-sitter as emacs dep

---
+++ tree-sitter.py
@@ -0,0 +1,44 @@
+from seth.formula import Formula
+from seth.types import BuildType
+
+class TreeSitterFormula(Formula):
+    name = "tree-sitter"
+    latest = "0.26.9"
+
+    # dependencies = []
+    # build_dependencies = []
+    build_system = BuildType.MAKE
+
+    versions = {
+        "0.26.9": {
+            "url": "https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v0.26.9.tar.gz",
+            "sha256": "8e14780500933f43d86662fcaa1b0ce99ebe9c220f4680bc929dce09a0e0cfc6",
+        },
+    }
+
+    # def configure_args(self):
+    #     return [
+    #         f"--prefix={self.keg}",
+    #         "--enable-shared",
+    #     ]
+
+    # def configure_args(self) -> list[str]:
+    #    return [f"--prefix={self.keg}"] + self.extra_configure_args
+
+    def make_args(self) -> list[str]:
+        return [
+            f"PREFIX={self.keg}",
+        ]
+
+    # def cmake_args(self) -> list[str]:
+    #    return [f"-DCMAKE_INSTALL_PREFIX={self.keg}"] + self.extra_configure_args
+
+    # def meson_args(self) -> list[str]:
+    #    return [f"--prefix={self.keg}"] + self.extra_configure_args
+
+    # def patch(self, source_dir: Path):
+    #     """Override for programmatic source modifications applied before build."""
+
+    # def post_install(self):
+    #     pass
+
